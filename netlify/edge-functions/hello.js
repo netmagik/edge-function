704023afc2112512fe7f3dc4dfa214fa572d7e67
Irina <irina@netmagik.com>
2022-12-20 13:35:05 -0500

    Add log

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,4 +1,6 @@
 export default async (request, context) => {
+    context.log('Hello - Calling Hello.js Edge Function')
+
     return new Response('Hello World from The Edge!', {
         headers: {
             "content-type": "text/html",
